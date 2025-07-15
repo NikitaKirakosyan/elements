d7f81e251264f51e26d5257a6da34124636ae2c8
Nikita Kirakosyan <nikita.kirakosyan.work@gmail.com>
2025-07-15 15:48:02 +0300

    BlockController base implementation

---
+++ Assets/_elements/Features/Core/Scripts/BlockController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BlockController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private float _minSwipe = 0.5f;
+    
+    private Vector2 _touchStart;
+    
+    public Vector2 PositionOnGrid { get; private set; }
+    
+    
+    void IPointerDownHandler.OnPointerDown(PointerEventData data)
+    {
+        _touchStart = data.position;
+    }
+    
+    void IPointerUpHandler.OnPointerUp(PointerEventData data)
+    {
+        //Do swipe logic
+        var diff = data.position - _touchStart;
+        if (diff.magnitude < _minSwipe * Screen.dpi)
+            return;
+        
+        diff.Normalize();
+    }
+    
+    
+    public void Init(Vector2 positionOnGrid)
+    {
+        PositionOnGrid = positionOnGrid;
+    }
+    
+    public void MoveToCell(Vector2 positionOnGrid)
+    {
+        PositionOnGrid = positionOnGrid;
+        //TODO do move
+    }
+    
+    public void SetCoords(Vector2 positionOnGrid)
+    {
+        PositionOnGrid = positionOnGrid;
+    }
+    
+    public void Die()
+    {
+        Destroy(gameObject); //TODO add animation
+    }
+}
